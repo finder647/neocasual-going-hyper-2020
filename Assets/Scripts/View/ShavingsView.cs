91dee71ccc5bedcc3fd7f6cd9100275cc4d29228
Wahyu Hidayat <wahyuwhyhidayat@gmail.com>
2020-06-20 14:01:18 +0700

    Improve Swipe to Rotation

--- Assets/Scripts/View/ShavingsView.cs
+++ Assets/Scripts/View/ShavingsView.cs
@@ -4,25 +4,25 @@ namespace NeoCasual.GoingHyper
 {
     public class ShavingsView : MonoBehaviour
     {
-        public float swipeDistanceToRotation = 10f;
+        public float swipeDistanceToRotation = 20f;
+        [Range (0f, 1f)] public float minDeltaSwipePosX = 0.05f;
 
-        private Vector3 _prevHoldPosition;
+        private Vector3 _prevHoldPos;
 
         public void OnStartSwiping (Vector2 position)
         {
-            _prevHoldPosition = position;
+            _prevHoldPos = position;
         }
 
         public void OnSwiping (Vector2 position, Vector2 distance)
         {
+            float deltaPosX = Mathf.Abs (_prevHoldPos.x - position.x) / Screen.width;
             Vector3 rotationForce = Vector3.zero;
-            if (_prevHoldPosition.x != position.x)
-            {
-                rotationForce = new Vector3 (
-                    0f, Mathf.Abs (distance.x) * swipeDistanceToRotation, 0f
-                );
 
-                _prevHoldPosition = position;
+            if (deltaPosX > minDeltaSwipePosX)
+            {
+                rotationForce = new Vector3 (0f, deltaPosX * swipeDistanceToRotation, 0f);
+                _prevHoldPos = position;
             }
 
             transform.eulerAngles += rotationForce;
