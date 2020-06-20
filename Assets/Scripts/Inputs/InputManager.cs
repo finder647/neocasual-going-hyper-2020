fb0f1fc83428dfe1a9a7e1eeddd6c06d4669c603
Wahyu Hidayat <wahyuwhyhidayat@gmail.com>
2020-06-20 11:44:17 +0700

    Add Base Main

---
+++ Assets/Scripts/Inputs/InputManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+namespace NeoCasual.GoingHyper.Inputs
+{
+    public class InputManager
+    {
+        private InputConfig _config;
+
+        private Vector2 _mouseOrigin;
+        private bool _isSwiping;
+
+        public InputManager ()
+        {
+            _config = ResourceManager.GetScriptable<InputConfig> ("Input");
+        }
+
+        public void CheckUpdate ()
+        {
+            if (Input.GetMouseButtonDown (0))
+            {
+                StartSwipe ();
+            }
+
+            if (Input.GetMouseButton (0))
+            {
+                Swiping ();
+            }
+
+            if (Input.GetMouseButtonUp (0))
+            {
+                FinishSwipe ();
+            }
+        }
+
+        private void StartSwipe ()
+        {
+            _isSwiping = true;
+        }
+
+        private void Swiping ()
+        {
+
+        }
+
+        private void FinishSwipe ()
+        {
+            if (!_isSwiping)
+            {
+                return;
+            }
+
+            _isSwiping = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Main/Main.cs
@@ -0,0 +1,20 @@
+﻿using NeoCasual.GoingHyper.Inputs;
+using UnityEngine;
+
+namespace NeoCasual.GoingHyper
+{
+    public class Main : MonoBehaviour
+    {
+        private InputManager _input;
+
+        private void Awake ()
+        {
+            _input = new InputManager ();
+        }
+
+        private void Start ()
+        {
+            
+        }
+    }
+}
